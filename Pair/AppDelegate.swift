492c318c8fd4c0e24b3d80b901599ce6617c09dc
Chatan Konda <chatankonda@ucmerced-10-33-21-103.ucmerced.edu>
2017-04-07 14:14:59 -0700

    Firebase connected

--- Pair/AppDelegate.swift
+++ Pair/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
      
+        FIRApp.configure()
         
         
         return true
