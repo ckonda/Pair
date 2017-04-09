1145a86296616da05ad6a36110e05e102c4260b6
Chatan Konda <chatankonda@ucmerced-10-33-21-103.ucmerced.edu>
2017-04-09 16:47:22 -0700

    tableviews

---
+++ Pair/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Pair
+//
+//  Created by Chatan Konda on 4/9/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
+
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    var id: String?
+}
+
