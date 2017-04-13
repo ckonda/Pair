4bc91d7e0624308b7214c6fae265067b924588ae
Chatan Konda <chatankonda@ucmerced-10-33-21-103.ucmerced.edu>
2017-04-13 16:06:50 -0700

    login segue

---
+++ Pair/postBidViewController.swift
@@ -0,0 +1,25 @@
+//
+//  postBidViewController.swift
+//  Pair
+//
+//  Created by Chatan Konda on 4/12/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+class postBidViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+
+    }
+
+    
+
+
+
+}
---
+++ Pair/postComposeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  postComposeViewController.swift
+//  Pair
+//
+//  Created by Chatan Konda on 4/12/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+class postComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+    
+
+}
