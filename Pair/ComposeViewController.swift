520a50779000d8ccb4fd5af377d8d7c7cf12c5af
Rahul Vemuri <rvemuri@ucmerced.edu>
2017-04-16 15:22:22 -0700

    made Compose logic

--- Pair/ComposeViewController.swift
+++ Pair/ComposeViewController.swift
@@ -23,17 +23,18 @@ class ComposeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ref = FIRDatabase.database().reference()
+        ref = FIRDatabase.database().reference() //.child("Jobs").childByAutoId()
+   
        
-
+        
         // Do any additional setup after loading the view.
     }
 
     @IBOutlet weak var jobType: UITextField!//box1
     @IBOutlet weak var jobDescription: UITextField!//box2
     @IBOutlet weak var jobPrice: UITextField!//box3
-    
-    
+    //@IBOutlet weak var jobTitle: UITextField!//box3
+    //@IBOutlet weak var username: UITextField!//box3
     
     
     @IBAction func addPost(_ sender: Any) {
@@ -41,26 +42,38 @@ class ComposeViewController: UIViewController {
         //TO DO: post jobData to Firebase
         //dismiss the view
     //ref?.child("Jobs").childByAutoId().setValue(jobDescription)
-        
+        //let userID = ref?.childByAutoId().key
         presentingViewController?.dismiss(animated: true, completion: nil)
         let job1 = jobType.text!
         let job2 = jobDescription.text!
         let job3 = jobPrice.text!
+        //let job4 = username.text!
+    
+        
       //  let ref = FIRDatabase.database().reference()
+        let postData = [
+        "title": job1,
+        //"job": "a", //as! NSString,
+        "price": job2, //as! NSString,
+        //"username": job4,
+        "description": job2
+        ] as [String : Any]
+        /*self.ref?.child("Jobs").child(userID!).setValue("jobtype", forUndefinedKey: "a")
+        self.ref?.child("Jobs").child(userID!).setValue("jobdescription", forUndefinedKey: "b")
+        self.ref?.child("Jobs").child(userID!).setValue("jobtype", forUndefinedKey: "c")*/
+        self.ref?.child("Jobs").childByAutoId().setValue(postData)
         
-        
-        let usersReference  = ref?.child("Users").child(uid)
         
     
         
-        jobType.text = ""
-        jobDescription.text = ""
-        jobPrice.text = ""
+        //jobType.text = ""
+        //jobDescription.text = ""
+        //jobPrice.text = ""
         
-        ref = FIRDatabase.database().reference()
-        ref?.child("Jobs").child(job1)
-        ref?.child("Jobs").child(job1).setValue(["Descriptions": job2])
-        ref?.child("Jobs").child(job1).setValue(["Price": job3])
+        //self.ref?.child("Jobs").child("ref").setValue("thing")
+        //newRef.childByAutoId().setValue(job1)
+       // ref?.child("Jobs").child(job1).setValue(["Descriptions": job2])
+        //ref?.child("Jobs").child(job1).setValue(["Price": job3])
         
     }
 
